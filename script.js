2d5bb88cf14273527179224c7b3c9f2234dd6f67
DBEGEAC <168447172+DBEGEAC@users.noreply.github.com>
2025-04-15 01:19:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const loginForm = document.getElementById("loginForm");
+const message = document.getElementById("message");
+
+loginForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  if (username === "admin" && password === "1234") {
+    message.textContent = "Login successful!";
+    message.style.color = "green";
+  } else {
+    message.textContent = "Invalid username or password.";
+    message.style.color = "red";
+  }
+});
